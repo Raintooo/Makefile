15905cfc5825a555e70d162f4d746a3d162fcb36
zhu_joee <674195564@qq.com>
2020-01-09 16:21:04 +0800

    增加模块的单独编译; 完善记录

--- pro/pro-rule.mk
+++ pro/pro-rule.mk
@@ -1,5 +1,4 @@
-.PHONY : clean rebuild compile link
-
+.PHONY : clean rebuild compile link $(MODULE)
 
 DIR_PROJECT := $(realpath .)
 
@@ -10,6 +9,18 @@ MODULE_LIB := $(addprefix $(DIR_BUILD)/,$(MODULE_LIB))
 
 APP := $(addprefix $(DIR_BUILD)/,$(APP)) 
 
+
+define MAKE_MODULE
+	cd $(1) \
+	&& make all DEBUG:=$(DEBUG) \
+	DIR_BUILD:=$(addprefix $(DIR_PROJECT)/,$(DIR_BUILD))\
+	DIR_COMMON_INC:=$(addprefix $(DIR_PROJECT)/,$(DIR_COMMON_INC))\
+	MODULE_CFG:=$(addprefix $(DIR_PROJECT)/,$(MODULE_CFG))\
+	MODULE_CMD:=$(addprefix $(DIR_PROJECT)/,$(MODULE_CMD))\
+	MODULE_RULE:=$(addprefix $(DIR_PROJECT)/,$(MODULE_RULE))\
+	&& cd ..;
+endef
+
 all: compile $(APP)
 	@echo "success!! output => $(APP)"
 
@@ -18,25 +29,21 @@ compile : $(DIR_BUILD) $(DIR_BUILD_SUB)
 	@set -e;\
 	for dir in $(MODULE);\
 	do\
-		cd $$dir \
-		&& make all DEBUG:=$(DEBUG) \
-		DIR_BUILD:=$(addprefix $(DIR_PROJECT)/,$(DIR_BUILD))\
-		DIR_COMMON_INC:=$(addprefix $(DIR_PROJECT)/,$(DIR_COMMON_INC))\
-		MODULE_CFG:=$(addprefix $(DIR_PROJECT)/,$(MODULE_CFG))\
-		MODULE_CMD:=$(addprefix $(DIR_PROJECT)/,$(MODULE_CMD))\
-		MODULE_RULE:=$(addprefix $(DIR_PROJECT)/,$(MODULE_RULE))\
-		&& cd ..;\
+		$(call MAKE_MODULE, $$dir)\
 	done
 	
 	@echo "compile OK"
+	
+$(MODULE) : $(DIR_BUILD) $(DIR_BUILD)/$(MAKECMDGOALS)
+	$(call MAKE_MODULE, $(MAKECMDGOALS))
 	
 $(DIR_BUILD_SUB) $(DIR_BUILD) : 
 	$(MKDIR) $@
 	
 link $(APP) :  $(MODULE_LIB)
-	$(CC) $(LFLAGS) -o $(APP) -Xlinker "-(" $^ -Xlinker "-)" 
-	
+	$(CC) $(LFLAGS) -o $(APP) -Xlinker "-(" $^ -Xlinker "-)" 	
+
 clean : 
-	$(RM) $(DIR_BUILD) $(APP)
+	$(RM) $(DIR_BUILD)
 	
 rebuild : clean all
